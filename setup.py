758db5d908cd0abc0b5315afded474f5d6daf3d5
ukrolelo <60402662+ukrolelo@users.noreply.github.com>
2021-08-22 11:24:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AirzoneCloud",
-    version="0.5.0",
+    version="0.5.0.1",
     author="max13fr",
     author_email="max13fr@yozo.fr",
     description="Python3 library for Airzone Cloud API",
